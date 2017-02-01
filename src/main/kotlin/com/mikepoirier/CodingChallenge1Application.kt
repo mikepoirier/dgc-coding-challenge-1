1e03e9046963319c0fbb2dad25f71b0d9500be6e
Michael Poirier <mikepoirier26@gmail.com>
2017-02-01 16:06:35 -0700

    Initial commit

---
+++ src/main/kotlin/com/mikepoirier/CodingChallenge1Application.kt
@@ -0,0 +1,11 @@
+package com.mikepoirier
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CodingChallenge1Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(CodingChallenge1Application::class.java, *args)
+}
---
+++ src/test/kotlin/com/mikepoirier/CodingChallenge1ApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.mikepoirier
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CodingChallenge1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
